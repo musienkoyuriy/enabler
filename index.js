020ecb69fe50c4fa23d5b5937badd5bbd2e2e91b
musienkoyuriy <musienkoyuriy@gmail.com>
2018-03-12 23:13:46 +0200

    up version in commander

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const program = require('commander');
 
 program
-  .version('1.0.8')
+  .version('1.1.0')
   .option('-P, --path <s>', 'Path for your root components folder')
   .option('--ng', 'Detect angular abstractions')
   .parse(process.argv);
