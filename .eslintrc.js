d9d084f203457e23c0180c1e359fd7e6742cc0ea
musienkoyuriy <musienkoyuriy@gmail.com>
2018-03-19 22:53:18 +0200

    fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     'prefer-promise-reject-errors': 0,
-    'no-underscore-dangle': 0,
-    'class-methods-use-this': false
+    'no-underscore-dangle': 0
   },
 };
