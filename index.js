44745d2dd8272bb8d29b30d337f54e3ab78362df
musienkoyuriy <musienkoyuriy@gmail.com>
2018-03-25 15:26:38 +0300

    add support for vue binding and events

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ program
   .version('1.2.2')
   .option('-P, --path <s>', 'Path for your root components folder')
   .option('--ng', 'Detect angular abstractions')
+  .option('--vue', 'Parse vue.js files')
   .parse(process.argv);
 
 global.framework = program.ng ? 'angular' : undefined;
+
+if (program.ng) {
+  global.framework = 'angular';
+} else if (program.vue) {
+  global.framework = 'vue';
+}
 
 module.exports = require('./lib')(program);
