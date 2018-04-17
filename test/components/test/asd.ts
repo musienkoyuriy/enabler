30618d221a8566f02b021df91c2756a3916f39c1
musienkoyuriy <musienkoyuriy@gmail.com>
2018-04-17 15:17:57 +0300

    support typescript files in angular mode

---
+++ test/components/test/asd.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  template: `
+
+    <div>sdfsdfsdf</div>
+    <label></label>
+
+
+    <basefont></basefont>
+
+  `
+})
+export class asd {}
