69e2e3af2ab194559025623445466241fff6b5b5
musienkoyuriy <musienkoyuriy@gmail.com>
2018-04-23 11:46:27 +0300

    1.4.2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const program = require('commander');
 
 program
-  .version('1.4.1')
+  .version('1.4.2')
   .option('-P, --path <s>', 'Path for your root components folder')
   .option('--ng', 'Detect angular abstractions')
   .option('--vue', 'Support .vue files')
