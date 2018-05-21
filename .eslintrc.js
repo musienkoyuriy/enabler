a83479ff16068e34283921c3be7d558984cf5c9c
musienkoyuriy <musienkoyuriy@gmail.com>
2018-05-21 15:49:33 +0300

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,20 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: 'airbnb-base',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: ['airbnb-base', 'prettier'],
   rules: {
     'prefer-promise-reject-errors': 0,
-    'no-underscore-dangle': 0
+    'no-underscore-dangle': 0,
+    'prettier/prettier': 'error'
   },
+  plugins: [
+    'prettier'
+  ],
+  env: {
+    'es6': true,
+    'node': true
+  }
 };
