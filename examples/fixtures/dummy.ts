d71a9554ad8662863a2b70bb9cd8432875b70b2d
musienkoyuriy <musienkoyuriy@gmail.com>
2018-05-22 17:59:28 +0300

    add demo with html|ng|vue fixrures

---
+++ examples/fixtures/dummy.ts
@@ -0,0 +1,29 @@
+// @ts-ignore
+@Component({
+  selector: 'example',
+  template: `
+    <h1>2</h1>
+    <h3>2</h1>
+    <h2>2</h2>
+    <h2>sd</h2>
+    <input type="text" placeholder="=sdf" id="sdf">
+    <img src="" [alt]="s">
+    <area shape="" coords="" href="" >
+    <textarea name="" id="56" cols="30" rows="10"></textarea>
+    <h4>sad</h4>
+    <audio src="" [autoplay]></audio>
+    <a [href]="#"></a>
+    <div (click)="" onkeydown="sdfs"></div>
+    <input type="text">
+    <div role="tablist">
+      <div role="ta"></div>
+    </div>
+    <html [lang]=""></html>
+    <div tabindex="-1"></div>
+    <input type="submit" [value]="" id="asd">
+    <div (mouseover)="f"></div>
+    <basefont></basefont>
+  `
+})
+// @ts-ignore
+export class ExampleComponent {}
---
+++ examples/fixtures/dummy.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>1</h1>
+    <h3>3</h3>
+    <h2>2</h2>
+    <h2>2</h2>
+    <input type="text" placeholder="=sdf" id="sdf">
+    <img src="" v-bind:alt="text">
+    <area shape="" coords="" href="" >
+    <textarea name="" id="56" cols="30" rows="10"></textarea>
+    <h4>sad</h4>
+    <audio src="" autoplay></audio>
+    <a href="#"></a>
+    <div @click="f" @keydown="f"></div>
+    <input type="text">
+    <div role="tablist">
+      <div role="ta"></div>
+    </div>
+    <html></html>
+    <div tabindex="-1"></div>
+    <input type="submit" v-bind:value="text" id="asd">
+    <div @mouseover="f"></div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: ''
+      }
+    },
+    methods: {
+      f() {}
+    }
+  }
+</script>
+
