04b65bc325ffd4985d8d66645814a569f6834f62
musienkoyuriy <musienkoyuriy@gmail.com>
2018-09-07 14:58:47 +0300

    up commander

--- src/parser.ts
+++ src/parser.ts
@@ -64,7 +64,7 @@ export function getTemplateFromComponentDecorator(fileContent: string): string {
     matchedString,
     ''
   );
-
+ 
   const angularTemplate = stringExceptTemplateLiteral.slice(
     0,
     stringExceptTemplateLiteral.indexOf('`')
