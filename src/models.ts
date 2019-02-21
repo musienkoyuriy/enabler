8825b581b0bf6728f7bef00ad58e981ad45cd215
musienkoyuriy <musienkoyuriy@gmail.com>
2019-02-21 15:12:44 +0200

    fix warningMessage typing

--- src/models.ts
+++ src/models.ts
@@ -4,14 +4,14 @@ export interface ValidatorOptions {
   isInvalid: ($elem: Cheerio, attrs?: string[], events?: string[]) => boolean;
   selectors: string[] | string;
   $template: any;
-  warningMessage: string | ((el: any) => string);
+  warningMessage: string | ((el: Cheerio) => string);
   content: string;
   assocAttrs?: string[];
   assocEvents?: string[];
 }
 
 export type ValidatorFactory = ($: any, content: string, options: ProgramOptions) => Validator;
-export type TemplatesWithWarnings = {[key: string]: Warning[]};
+export interface TemplatesWithWarnings {[key: string]: Warning[];}
 
 export interface Warning {
   message: string;
