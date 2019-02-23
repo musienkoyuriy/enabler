c2ec4f6448f63c0e4bfcbb03d58085d19d86617a
musienkoyuriy <musienkoyuriy@gmail.com>
2019-02-23 17:42:54 +0200

    patch 2.4.1

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,7 @@ import program = require('commander');
 import { run } from '.';
 
 program
-  .version('2.3.0', '-v, --version')
+  .version('2.4.1', '-v, --version')
   .option('-p, --path <s>', 'Path for your root components folder')
   .option('--ng', 'Detect angular abstractions')
   .option('--vue', 'Support .vue files')
