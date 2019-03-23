6667dc1400f33a402fcc7e4d953bf7bf95ac71fd
musienkoyuriy <musienkoyuriy@gmail.com>
2019-03-23 12:35:34 +0200

    revert commented rules

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,51 +1,51 @@
-// import ariaUnsupportedElements from './aria-unsupported-elements';
-// import buttonHasType from './button-has-type';
-// import clickWithKeyboardEvent from './click-with-key-event';
-// import controlsFormWrapped from './controls-form-wrapped';
-// import hasAlt from './has-alt';
-// import headingHasContent from './heading-has-content';
-// import htmlHasLang from './html-has-lang';
-// import iFrameHasTitle from './iframe-has-title';
-// import labelHasFor from './label-has-for';
-// import linksHasUrl from './links-has-url';
-// import mouseEventsWithoutKeyEvents from './mouseevents-without-keyevents';
+import ariaUnsupportedElements from './aria-unsupported-elements';
+import buttonHasType from './button-has-type';
+import clickWithKeyboardEvent from './click-with-key-event';
+import controlsFormWrapped from './controls-form-wrapped';
+import hasAlt from './has-alt';
+import headingHasContent from './heading-has-content';
+import htmlHasLang from './html-has-lang';
+import iFrameHasTitle from './iframe-has-title';
+import labelHasFor from './label-has-for';
+import linksHasUrl from './links-has-url';
+import mouseEventsWithoutKeyEvents from './mouseevents-without-keyevents';
 import noMediaAutoplay from './no-media-autoplay';
-// import noDuplicatedAttributes from './no-duplicated-attributes';
-// import empty from './no-empty-links-and-buttons';
-// import noFormattingTags from './no-formatting-tags';
-// import noJumpingHeaders from './no-jumping-headers';
-// import noPositiveTabindex from './no-positive-tabindex';
-// import pageHasTitle from './page-has-title';
-// import placeholderHasLabel from './placeholder-has-label';
-// import rolesAreValid from './roles-are-valid';
-// import scopeOnlyInCell from './scope-only-in-cell';
-// import tabElementsHasRightRoles from './tab-elements-has-right-roles';
-// import textInputHasLabel from './text-input-has-label';
-// import unclickableWithoutRole from './unclickable-without-role';
+import noDuplicatedAttributes from './no-duplicated-attributes';
+import empty from './no-empty-links-and-buttons';
+import noFormattingTags from './no-formatting-tags';
+import noJumpingHeaders from './no-jumping-headers';
+import noPositiveTabindex from './no-positive-tabindex';
+import pageHasTitle from './page-has-title';
+import placeholderHasLabel from './placeholder-has-label';
+import rolesAreValid from './roles-are-valid';
+import scopeOnlyInCell from './scope-only-in-cell';
+import tabElementsHasRightRoles from './tab-elements-has-right-roles';
+import textInputHasLabel from './text-input-has-label';
+import unclickableWithoutRole from './unclickable-without-role';
 
 export {
-  // noJumpingHeaders,
-  // noFormattingTags,
-  // hasAlt,
-  // empty,
-  // placeholderHasLabel,
-  // htmlHasLang,
-  // mouseEventsWithoutKeyEvents,
-  // headingHasContent,
-  // clickWithKeyboardEvent,
-  // ariaUnsupportedElements,
+  noJumpingHeaders,
+  noFormattingTags,
+  hasAlt,
+  empty,
+  placeholderHasLabel,
+  htmlHasLang,
+  mouseEventsWithoutKeyEvents,
+  headingHasContent,
+  clickWithKeyboardEvent,
+  ariaUnsupportedElements,
   noMediaAutoplay,
-  // pageHasTitle,
-  // noDuplicatedAttributes,
-  // unclickableWithoutRole,
-  // noPositiveTabindex,
-  // labelHasFor,
-  // linksHasUrl,
-  // controlsFormWrapped,
-  // tabElementsHasRightRoles,
-  // textInputHasLabel,
-  // buttonHasType,
-  // iFrameHasTitle,
-  // scopeOnlyInCell,
-  // rolesAreValid
+  pageHasTitle,
+  noDuplicatedAttributes,
+  unclickableWithoutRole,
+  noPositiveTabindex,
+  labelHasFor,
+  linksHasUrl,
+  controlsFormWrapped,
+  tabElementsHasRightRoles,
+  textInputHasLabel,
+  buttonHasType,
+  iFrameHasTitle,
+  scopeOnlyInCell,
+  rolesAreValid
 };
