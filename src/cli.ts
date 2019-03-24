5afff805af7f3ca004313794fcc0b7eab1ff9930
musienkoyuriy <musienkoyuriy@gmail.com>
2019-03-24 03:42:36 +0200

    2.5.0

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,7 @@ import program = require('commander');
 import { run } from '.';
 
 program
-  .version('2.4.10', '-v, --version')
+  .version('2.5.0', '-v, --version')
   .option('-p, --path <s>', 'Path for your root components folder')
   .option('--ng', 'Detect angular abstractions')
   .option('--vue', 'Support .vue files')
